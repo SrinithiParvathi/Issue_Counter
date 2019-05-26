74cac6f22c72cf5b6576a950f97c9a133ac20793
uta-er <uta-er@eur.ad.sag>
2019-05-26 12:43:15 +0530

    Final commit

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
 from flask import Flask
-from flask import request
+from flask import request, render_template
 from flask import jsonify
 import json
 
 from application import *
 app = Flask(__name__)
 
-@app.route('/getIssues',methods=['POST'])
+
+@app.route('/')
+def login():
+    return render_template('login.html')                                       #main page to input link
+
+
+@app.route('/getIssues', methods=['POST'])
 def get_issues():
-   request_params = request.data
-   params = json.loads(request_params)
-   print(params['url'])
-   result = hit_url(params['url'])
-   print(result)
-   response = app.response_class(response=result, status=200, mimetype='application/json')
-   
-   return response
-   
-   
-   
+    url = request.form.to_dict()
+    res = hit_url(url['link'])                                                 #input link is passed through the url and passed to hit_url function to fetch the count
+    return render_template("result.html", result=json.loads(res))
 
 
 if __name__ == '__main__':
-   app.debug = True
-   app.run()
-   app.run(debug = True)
-   
+    app.debug = True
+    app.run()
+    app.run(debug=True)
--- application.py
+++ application.py
@@ -3,79 +3,67 @@ import requests
 from datetime import datetime
 from flask import json
 import json
-x = 0
-
-#GITHUB_USER = 'SrinithiParvathi'
-#GITHUB_PASSWORD = 'Welcome12326'
-# REPO = 'hunspell/hunspell'  # format is username/repo
-# ISSUES_FOR_REPO_URL = 'https://api.github.com/repos/%s/issues' % REPO
-#auth = None
-#AUTH = (GITHUB_USER, GITHUB_PASSWORD)
-BASE_URL = 'https://api.github.com/repos/'
+result = 0
+BASE_URL = 'https://api.github.com/repos/'                                                      #its a base url to fetch open issuses from github 
 END_POINT = '/issues'
+total = 0
+
+
+
+
+# Function to fetch result based on input url
 
 def hit_url(url):
-    REQUEST_URL =  BASE_URL + url + END_POINT
+    REQUEST_URL = BASE_URL + url + END_POINT                                                     #url is the input link from which issues has to be fetched
     print(REQUEST_URL)
     response = requests.get(REQUEST_URL)
-    #csvout.writerow(('id', 'Title', 'Body', 'Created At', 'Updated At'))
     count = write_issues(response)
 
-    if 'link' in response.headers:
-        pages = {rel[6:-1]: url[url.index('<') +1 : -1] for url, rel in
-                (link.split(';') for link in
-                response.headers['link'].split(','))}
-        while 'last' in pages and 'next' in pages:
-            pages = {rel[6:-1]: url[url.index('<') +1 : -1] for url, rel in
+    if 'link' in response.headers:                                                              #if there are more than one page
+        pages = {rel[6:-1]: url[url.index('<') + 1: -1] for url, rel in
                  (link.split(';') for link in
                   response.headers['link'].split(','))}
+        while 'last' in pages and 'next' in pages:
+            pages = {rel[6:-1]: url[url.index('<') + 1: -1] for url, rel in
+                     (link.split(';') for link in
+                      response.headers['link'].split(','))}
 
-            print(response.headers['link'])
             response = requests.get(pages['next'])
-            x = write_issues(response)
-            count += x
+            result = write_issues(response)                                                      #call write_issues - the response for the request 
+            count += result
             if pages['next'] == pages['last']:
                 break
-    return x
-    
+    return result
+	
+	
+
+# Function to process the response object parse it and counts the open issuses based on different filter
+
 def write_issues(response):
     count = 0
-    Listless_7 = list()
-    List2 = list()
-    List3 = list()
     count1 = 0
     count2 = 0
     count3 = 0
-    """Parses JSON response and writes to CSV."""
+    global total
     if response.status_code != 200:
-        raise Exception(response.status_code)
+        raise Exception(response.status_code)                                                  #if status code is not 200 exception is raised else response for issues is processed
     for issue in response.json():
         if 'pull_request' not in issue:
             labels = ', '.join([l['name'] for l in issue['labels']])
-            #print(issue['url'])
             issue_created_at = issue['created_at']
             issue_time = issue_created_at.split('T')[0]
             someday = datetime.strptime(issue_time, '%Y-%m-%d').date()
             today = datetime.now().date()
-            difference = today - someday
-            print(difference.days)
+            difference = today - someday                                                        #we calculate the open issues based one the filter (less tahn 24 hrs more tham  a week etc)
 
             if(difference.days < 1):
                 count1 = count1 + 1
-                Listless_7.append(issue['url'])
-            elif (difference.days < 7 and difference.days > 1) :
+            elif (difference.days < 7 and difference.days > 1):
                 count2 = count2 + 1
-                List2.append(issue['url'])
-            elif(difference.days > 3200):
+            elif(difference.days > 7):
                 count3 = count3 + 1
-                List3.append(issue['url'])
             count += 1
-
-    print(count1)
-    print(count2)
-    print(count3)
-
-    result = json.dumps({'count_1': count1, 'count_2': count2, 'count_3': count3})
-    print(result)
+    total += count
+    result = json.dumps({'Less than 24hrs': count1, 'More than 24hrs Less than week': count2,      # here we return an json object wih the counts to hit_url_url
+                         'More than 7 days': count3, 'Total count': total})
     return result
-            # Change the following line to write out additional fields
